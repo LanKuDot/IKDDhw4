3fe7d03a3bee24fa7789a5f51a14524a08e6eb05
Kun-Yi Li <lankudot@github.com>
2014-11-25 16:40:07 +0800

    Create data srtucture to manage the content

---
+++ HW4/CharacterPosition.java
@@ -0,0 +1,10 @@
+/* The data structure of the position of each character in all input file.
+ */
+package HW4;
+
+public class CharacterPosition
+{
+	public int fileID;
+	public int row;
+	public int column;
+}
