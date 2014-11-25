7c38694541c05c832c3e8cd1ad2cb8b21bc37c6a
Kun-Yi Li <lankudot@github.com>
2014-11-25 20:35:57 +0800

    Improve the coding style

--- HW4/PageLoader.java
+++ HW4/PageLoader.java
@@ -9,23 +9,23 @@ import java.util.Hashtable;
 
 public class PageLoader
 {
-	private String[] pagePaths;		/* All paths of the text files */
-	private Hashtable< Character, ArrayList<CharacterPosition> > charPos;
+	private String[] pagePaths_;		/* All paths of the text files */
+	private Hashtable< Character, ArrayList<CharacterPosition> > charPos_;
 
 	/* Constructor: Store the paths which are user assigned,
 	 * and create new hash table to store the information of the content.
 	 */
 	public PageLoader( String[] filePaths )
 	{
-		pagePaths = filePaths;
-		charPos = new Hashtable< Character, ArrayList<CharacterPosition> >();
+		pagePaths_ = filePaths;
+		charPos_ = new Hashtable< Character, ArrayList<CharacterPosition> >();
 
 	}	// end of constructor PageLoader( String )
 
 	public void loadFiles()
 	throws FileNotFoundException, IOException
 	{
-		for ( int i = 0; i < pagePaths.length; ++i )
+		for ( int i = 0; i < pagePaths_.length; ++i )
 			loadContentToHashTable( i );
 
 	}	// end of fileContent() function
@@ -36,7 +36,7 @@ public class PageLoader
 	private void loadContentToHashTable( int fileIndex )
 	throws FileNotFoundException, IOException
 	{
-		BufferedReader reader = new BufferedReader( new FileReader( pagePaths[fileIndex] ) );
+		BufferedReader reader = new BufferedReader( new FileReader( pagePaths_[fileIndex] ) );
 		ArrayList< CharacterPosition > tmpList;
 		String stringNow;
 		int rowNow = 0;
@@ -54,13 +54,13 @@ public class PageLoader
 
 					// If the table has already contained the element, get the old arraylist.
 					// Otherwise, create a new one.
-					if ( charPos.containsKey( aChar ) )
-						tmpList = charPos.get( aChar );
+					if ( charPos_.containsKey( aChar ) )
+						tmpList = charPos_.get( aChar );
 					else
 						tmpList = new ArrayList< CharacterPosition >();
 
 					tmpList.add( new CharacterPosition( fileIndex, rowNow, columnNow ) );
-					charPos.put( aChar, tmpList );
+					charPos_.put( aChar, tmpList );
 				}
 		}
 
