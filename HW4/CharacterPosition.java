a5c7ccab0c7a329ed3d3c21dfc0a21a3335b74ab
Kun-Yi Li <lankudot@github.com>
2014-11-25 19:42:11 +0800

    Add constructor to the CharacterPosition

--- HW4/CharacterPosition.java
+++ HW4/CharacterPosition.java
@@ -7,4 +7,11 @@ public class CharacterPosition
 	public int fileID;
 	public int row;
 	public int column;
+	
+	public CharacterPosition( int fileID, int row, int column )
+	{
+		this.fileID = fileID;
+		this.row = row;
+		this.column = column;
+	}
 }
